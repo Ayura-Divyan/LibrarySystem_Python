8299589e8f56455295c697e39722b6c2f98b252e
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-01 14:35:15 +0530

    load_student function created

--- src/Main.py
+++ src/Main.py
@@ -1,9 +1,11 @@
 import csv
 
-books = {} #Initialised a books dictionary to store the loaded book data
+#Initialised dictionaries to load the data to
+books = {}
+student = {}
 
 def load_book():
-    """Used to load the csv files into python memory"""
+    """Used to load the book.csv files into python memory"""
     try:
         with open('../data/book.csv', encoding="utf-8") as csvfile:
             reader = csv.reader(csvfile) #Loads the csv file into a variable
@@ -26,6 +28,27 @@ def load_book():
             print("Books loaded")
     except FileNotFoundError: #Catch
         print("Books file not found")
+
+def load_student():
+    """Used to load the student.csv files into python memory"""
+    try:
+        with open('../data/student.csv', encoding="utf-8") as csvfile:
+            reader = csv.reader(csvfile) #Loads the csv file into a variable
+
+            next(reader) #Skips the header row
+
+            for row in reader:
+                if len(row) == 2:
+                    #Initialise the rows to separate indexes
+                    student_id = row[0]
+                    first_name = row[1]
+
+                    #Initialise dictionary
+                    student[student_id] = {"first_name": first_name}
+
+            print("Students loaded")
+    except FileNotFoundError:
+        print("Students file not found")
 
 #Start script
 if __name__ == "__main__":
