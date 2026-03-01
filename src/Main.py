974d9ba1dc5d3101540a6ac1d52d7aacd61c43e5
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-01 14:51:54 +0530

    Transaction loader created, transaction id added to transactions.csv

--- src/Main.py
+++ src/Main.py
@@ -3,6 +3,7 @@ import csv
 #Initialised dictionaries to load the data to
 books = {}
 student = {}
+transaction = {}
 
 def load_book():
     """Used to load the book.csv files into python memory"""
@@ -47,13 +48,39 @@ def load_student():
                     student[student_id] = {"first_name": first_name}
 
             print("Students loaded")
-    except FileNotFoundError:
+    except FileNotFoundError: #Catch
         print("Students file not found")
+
+def load_transaction():
+    """Used to load the transactions.csv files into python memory"""
+    try:
+        with open('../data/transactions.csv', encoding="utf-8") as csvfile:
+            reader = csv.reader(csvfile) #Loads the csv file into a variable
+
+            next(reader) #Skips the header row
+
+            for row in reader:
+                if len(row) == 2:
+                    #Initialise the rows to separate indexes
+                    transaction_id = row[0]
+                    date = row[1]
+                    book_id = row[2]
+                    student_id = row[3]
+                    transaction_type = row[4]
+
+                    #Initialise dictionary
+                    transaction[transaction_id] = {"date": date, "book_id": book_id, "student_id": student_id, "type": transaction_type}
+
+            print("Transactions loaded")
+    except FileNotFoundError: #Catch
+        print("Transaction file not found")
 
 #Start script
 if __name__ == "__main__":
     load_book()
     load_student()
+    load_transaction()
 
     print(books)
     print(student)
+    print(transaction)
