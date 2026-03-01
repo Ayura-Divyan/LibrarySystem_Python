a774cd49c0715c024426603e136c2d1ebd089f38
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-01 11:59:57 +0530

    Directory fixed

--- src/Main.py
+++ src/Main.py
@@ -5,7 +5,7 @@ books = {} #Initialised a books dictionary to store the loaded book data
 def load_book():
     """Used to load the csv files into python memory"""
     try:
-        with open('/data/book.csv') as csvfile:
+        with open('../data/book.csv') as csvfile:
             reader = csv.reader(csvfile) #Loads the csv file into a variable
 
             for row in reader:
