3f833213476d3fc6933d4c863f938a1024634ae7
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-01 16:25:42 +0530

    Separate method made for warm boot

---
+++ src/warm_boot.py
@@ -0,0 +1,78 @@
+import csv
+
+#Initialised dictionaries to load the data to
+books = {}
+student = {}
+transaction = {}
+
+def load_book():
+    """Used to load the book.csv files into python memory"""
+    try:
+        with open('../data/book.csv', encoding="utf-8") as csvfile:
+            reader = csv.reader(csvfile) #Loads the csv file into a variable
+
+            next(reader) #Skips the header row
+
+            for row in reader:
+                #Initialise the rows to separate indexes
+                if len(row) == 6:
+                    book_id = row[0]
+                    isbn = row[1]
+                    title = row[2]
+                    copies = row[3]
+                    availability = row[4]
+                    price = row[5]
+
+                    #Initialise dictionary
+                    books[book_id] = {"title": title, "isbn": isbn, "copies": int(copies), "availability": availability, "price": float(price)}
+
+            print("Books loaded")
+            return books
+    except FileNotFoundError: #Catch
+        print("Books file not found")
+
+def load_student():
+    """Used to load the student.csv files into python memory"""
+    try:
+        with open('../data/student.csv', encoding="utf-8") as csvfile:
+            reader = csv.reader(csvfile) #Loads the csv file into a variable
+
+            next(reader) #Skips the header row
+
+            for row in reader:
+                if len(row) == 2:
+                    #Initialise the rows to separate indexes
+                    student_id = row[0]
+                    first_name = row[1]
+
+                    #Initialise dictionary
+                    student[student_id] = {"first_name": first_name}
+
+            print("Students loaded")
+            return student
+    except FileNotFoundError: #Catch
+        print("Students file not found")
+
+def load_transaction():
+    """Used to load the transactions.csv files into python memory"""
+    try:
+        with open('../data/transactions.csv', encoding="utf-8") as csvfile:
+            reader = csv.reader(csvfile) #Loads the csv file into a variable
+
+            next(reader) #Skips the header row
+
+            for row in reader:
+                if len(row) == 5:
+                    #Initialise the rows to separate indexes
+                    transaction_id = row[0]
+                    date = row[1]
+                    book_id = row[2]
+                    student_id = row[3]
+                    transaction_type = row[4]
+
+                    #Initialise dictionary
+                    transaction[transaction_id] = {"date": date, "book_id": book_id, "student_id": student_id, "type": transaction_type}
+            print("Transactions loaded")
+            return transaction
+    except FileNotFoundError: #Catch
+        print("Transaction file not found")
