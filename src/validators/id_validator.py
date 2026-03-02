4383bcbcdb9a47aa9db6221badd51f91ec7cb05f
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-02 11:02:52 +0530

    Booking validator and unique validator tested

--- src/validators/id_validator.py
+++ src/validators/id_validator.py
@@ -2,23 +2,23 @@ import csv
 
 def is_unique(id_value, csv_file_name):
     """
-    Checks if the value entered by user is unique.
+    Checks if the value entered by user is unique compared to the CSV file.
     :param id_value: The user input to be validated
     :param csv_file_name: The name of the csv file
     :return: Boolean to validate the value
     """
     try:
-        with open(f"../../data/{csv_file_name}", encoding="utf-8") as csv_file:
+        with open(f"../../data/{csv_file_name}", "r", encoding="utf-8") as csv_file:
             reader = csv.reader(csv_file)
             next(reader)
-
             for row in reader:
                 if id_value in row:
-                    return False
+                    raise ValueError #If the id already exists in the csv file it will raise the error
             return True
-
     except FileNotFoundError:
-        raise FileNotFoundError(f"Error: File '{csv_file_name}' could be not found.")
+        print(f"Error: File '{csv_file_name}' could be not found.")
+    except ValueError:
+        print(f"Error: {id_value} is not unique")
 
 def booking_id_validator(booking_id):
     """
@@ -28,8 +28,10 @@ def booking_id_validator(booking_id):
     """
     booking_id = str(booking_id)
     try:
-        if booking_id[:3].isalpha() and booking_id[-2:].isdigit():
+        if len(booking_id) == 4 and booking_id[:2].isalpha() and booking_id[-2:].isdigit():
             return True
-        raise False
+        else:
+            raise ValueError #If the id doesn't meet the validation it will raise the error
     except ValueError:
-        raise ValueError(f"Invalid booking id: '{booking_id}'")
+        print(f"Error: {booking_id} is not a valid booking id.")
+
