2c49bdd05205c180f3eb19ac3365f9cb2a683673
Ayura Divyan <ayuradivyan@gmail.com>
2026-03-02 16:46:04 +0530

    Logic fixed and checksum algorithm added

--- src/validators/book_validator.py
+++ src/validators/book_validator.py
@@ -1,10 +1,23 @@
 def isbn_valid(value):
     """
-    Validates ISBN number according to International isbn
+    Validates ISBN number according to International ISBN Standard.
     :param value:
     :return:
     """
-    value = str(value.replace("-", ""))
-    if len(value) == 13 and value.isdigit() and (value[:3] in ["978", "979"]):
-        return True
-    return False
+    digits = str(value).replace("-", "").replace(" ", "") #Cleaning out the isbn
+
+    if len(digits) != 13 or not digits.isdigit():
+        return False
+
+    if digits[:3] not in ["978", "979"]:
+        return False
+
+    #Checksum calculation
+    total = 0
+    for i, digit in enumerate(digits):
+        if i % 2 == 0:
+            total += int(digit) * 1
+        else:
+            total += int(digit) * 3
+
+    return total % 10 == 0
