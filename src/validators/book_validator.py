97957760b5163ce1f9fb9e9b009a225ed401763a
Ayura Divyan <ayuradivyan@gmail.com>
2026-04-27 03:30:18 +0530

    Title space validation issue fix

--- src/validators/book_validator.py
+++ src/validators/book_validator.py
@@ -30,6 +30,7 @@ def title_valid(title):
     :param title:
     :return:
     """
+    title = title.replace(" ", "")
     if len(title) > 20 or not title.isalpha(): #Title has to contain only letters with a max length of 20
         return False
     else:
